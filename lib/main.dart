0dbff49a67dcaa04cc8487de723e3f7a2ef48206
kucnea <kucnea@naver.com>
2022-04-05 23:51:36 +0900

    add navigator active

--- lib/main.dart
+++ lib/main.dart
@@ -59,7 +59,13 @@ class _HelloPageState extends State<HelloPage> {
               children: <Widget>[
                 Text(_message,  style: TextStyle(fontSize: 30)),
                 Text('$_counter',  style: TextStyle(fontSize: 30)),
-
+                RaisedButton(
+                    child: Text('화면이동'),
+                    onPressed: () => {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => CupertinoPage()))
+                    })
               ],
             )
         ));
