fd12bc6551ed68e35eacfe0cb7505aa5f981d23d
kucnea <kucnea@naver.com>
2022-08-10 21:25:45 +0900

    messages first step

--- lib/chat/message.dart
+++ lib/chat/message.dart
@@ -5,8 +5,20 @@ class Messages extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
 
-    return Column(
-
+    return Container(
+      child: Column(
+        children: [
+          Expanded(
+              child: ListView.builder(
+                reverse: true,
+                itemCount: 2,
+                itemBuilder: (context, index){
+                  return Text("첫번째 메세지");
+                },
+            ),
+          )
+        ],
+      ),
     );
   }
 
