c30f2be3165cdea2101582178487ddb0cdfdc51d
kucnea <kucnea@naver.com>
2022-08-14 20:59:24 +0900

    pick up photo from camera & reset

--- lib/camera/view_camera.dart
+++ lib/camera/view_camera.dart
@@ -29,6 +29,20 @@ class _ViewCameraState extends State<ViewCamera> {
     }
   }
 
+  Future pickImageFromCamera() async {
+    try{
+      final image = await ImagePicker().pickImage(source: ImageSource.camera);
+
+      if(image == null) return;
+
+      final imageTemp = File(image.path);
+
+      setState(()=> this.image = imageTemp);
+    } on PlatformException catch(e){
+      print("이미지를 불러오는데 실패했습니다. : $e");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -38,6 +52,17 @@ class _ViewCameraState extends State<ViewCamera> {
           color: Colors.grey,
           child: Text("갤러리에서 사진 가져오기"),
           onPressed: (){ pickImageFromGallery(); },
+        ),
+        MaterialButton(
+          color: Colors.grey,
+          child: Text("카메라로 사진 찍기"),
+          onPressed: (){ pickImageFromCamera(); },
+        ),
+        MaterialButton(
+          color: Colors.grey,
+          child: Text("초기화"),
+          onPressed: (){
+            setState(()=> this.image = null); },
         ),
         SizedBox(height: 30,),
         image != null? Image.file(image!) : Text("선택된 이미지가 없습니다.", style: TextStyle(fontSize: 30.0),),
