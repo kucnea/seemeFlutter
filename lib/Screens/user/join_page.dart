767b4f2d41523c9945649d04f9fd6f79f7d0374b
kucnea <kucnea@naver.com>
2022-08-16 23:11:08 +0900

    connect to backend. but page auto move error

--- lib/Screens/user/join_page.dart
+++ lib/Screens/user/join_page.dart
@@ -20,12 +20,14 @@ class JoinPage extends StatefulWidget {
 
 
 // 데이터 통신을 위한 부분
-Future<ViewerModel?> addViewer(
+// Future<ViewerModel> addViewer(
+Future<String> addViewer(
   String vId, String vPw, BuildContext context) async{
-   var Url = "http://localhost:8060/addviewer";
-   // var Url = "http://127.0.0.1:8060/addviewer";
+   // var Url = "http://localhost:8060/addviewer";
+   // 안드로이드 에뮬의 localhost가 pc와 다르므로 하기 url이용
+   var Url = "http://10.0.2.2:8060/addviewer";
    var response = await http.post(Uri.parse(Url),
-   headers:<String, String>{"Content_Type":"application/json"},
+   headers:<String, String>{"Content-Type":"application/json"},
    body:jsonEncode(<String, String>{
      "vid":vId,
      "vpw":vPw,
@@ -36,16 +38,23 @@ Future<ViewerModel?> addViewer(
      showDialog(context: context,
          barrierDismissible: true,
          builder: (BuildContext dialogContext){
-          return MyAlertDialog(title: 'Backend Response', content: response.body);
+          // print("test responseBody Check");
+          // print(response.body);
+          return MyAlertDialog(title: '알람', content: response.body);
          },
      );
+   }else{
+     print("http status is not 200 ok");
+     print(response.statusCode);
    }
 
+   return response.body;
 }
 
 class _JoinPage extends State<JoinPage> {
 
   final _formKey = GlobalKey<FormState>();
+  bool? addCheck;
 
   TextEditingController idController = TextEditingController();
   TextEditingController pwController = TextEditingController();
@@ -98,20 +107,25 @@ class _JoinPage extends State<JoinPage> {
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: CustomElevatedButton(text: "회원가입", fnPageRoute: () async {
-                      print("Pressed addViewer Button.");
+                      // print("Pressed addViewer Button.");
                       String vId = idController.text;
                       String vPw = idController.text;
                       ViewerModel? viewerModel = null;
+                      String result = "";
+
                       if(_formKey.currentState!.validate()){
                         print("validate stage");
-                        viewerModel = await addViewer(vId, vPw, context);
+                        // viewerModel = await addViewer(vId, vPw, context);
+                        result = await addViewer(vId, vPw, context);
                         print(viewerModel?.vId);
                       }
                       vId = "";
                       vPw = "";
-                      setState(() {
-                        _viewerModel = viewerModel;
-                      });
+                      // setState(() {
+                      //   _viewerModel = viewerModel;
+                      // });
+
+                      result == "회원 가입에 성공했습니다."? Get.to(LoginFormPage()) : null;
 
                     }),
                   ),
@@ -148,10 +162,13 @@ class MyAlertDialog extends StatelessWidget {
         this.title,
         style: Theme.of(context).textTheme.titleLarge,
       ),
-      actions: this.actions,
+      actions: [
+        FlatButton(onPressed: () => Navigator.pop(context, true), child: Text("확인", style: TextStyle(fontSize: 20.0),))
+      ],
       content: Text(
         this.content,
-        style: Theme.of(context).textTheme.bodyLarge,
+        // style: Theme.of(context).textTheme.bodyLarge,
+        style: TextStyle(fontSize: 25.0),
       ),
     );
   }
